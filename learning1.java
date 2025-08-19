a30c0d8d5c0fae95ecafe96f8419a3f8ff2e9566
Cristian Xavier <cristiandsxavier@gmail.com>
2025-08-19 13:39:49 -0300

    Updating learning1

--- learning1.java
+++ learning1.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 public class learning1 {
     public static void main(String[] args){
-        System.out.println("Obey me world!");
+        System.out.println("Give me an integer: ");
+        Scanner entry = new Scanner(System.in);
+        int answer0 = entry.nextInt();
+        System.out.print("The number is: ");
+        System.out.println(answer0);
+        System.out.print("Give me a string: ");
+        entry.nextLine();
+        String answer1 = entry.nextLine();
+        System.out.print(answer1);  
+
+        entry.close();
     }
 }
