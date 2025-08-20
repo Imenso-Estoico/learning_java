d3e5cef996e50525526ceb90a366dbb2dd3b3b97
Cristian Xavier <cristiandsxavier@gmail.com>
2025-08-20 12:16:25 -0300

    Changing learning3 and adding learning4

--- learning3.java
+++ learning3.java
@@ -2,5 +2,5 @@ public class learning3 {
     public static void main(String[] args){
             String holder = learning2.answer(1);
             System.out.println(holder);
-        }
+    }
 }
---
+++ learning4.java
@@ -0,0 +1,15 @@
+//Questão de concurso que eu queria rodar
+
+public class learning4 {
+    public static void exibir(Integer valor){
+        System.out.println("Valor inteiro: " + valor);
+    }
+
+    public static void exibir(double valor){
+        System.out.println("Valor decimal: " + valor);
+    }
+
+    public static void main(String[] args){
+        exibir(null);
+    }
+}
